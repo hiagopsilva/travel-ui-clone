700e4c05ae72b30ed2ca4488ff32cc91d7e53f38
hiagopsilva <hiagohps360@gmail.com>
2020-10-17 15:07:49 -0300

    chore: updated styles global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,4 +7,15 @@ export default createGlobalStyle`
     outline: 0;
     box-sizing: border-box;
   }
+
+  body {
+  background: #FFFF;
+}
+
+  body, input, button, textarea {
+  font: 600 18px Inter, sans-serif;
+}
+  button {
+    cursor: pointer;
+  }
 `;
