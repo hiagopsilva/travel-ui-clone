44ae0356c72cd5fce564e0cb104b89812de0abd3
hiagopsilva <hiagohps360@gmail.com>
2020-10-17 13:25:22 -0300

    feat: added theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+export default {
+  Title: '#202336',
+  Button: '#FB8F1D',
+  background: '#FFFF',
+  text: '#7D7987',
+  textButtonPrimary: '#FFFF',
+  textButtonSecondary: '#FB8F1D',
+  optionMenu: '#B8BECD',
+  optionMenuActive: '#202336',
+  backgroundFooter: '#F9F9FB',
+  textSelect: '#4A4C53',
+  borderSelect: '#AFB0B9',
+};
